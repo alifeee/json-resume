23be15242efaec1e86d72321156c42eb5545b77f
alifeee <alifeeerenn@hotmail.com>
2023-06-14 00:09:20 +0100

    attempt to wait for image to load first, "properly" then, with a 5 second timeout. :(

--- print.js
+++ print.js
@@ -10,12 +10,31 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+async function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 (async () => {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
   await page.goto(`file://${__dirname}/resume.html`, {
     waitUntil: "networkidle0",
   });
+  // wait for images - https://stackoverflow.com/a/49233383
+  await page.evaluate(async () => {
+    await Promise.all(
+      Array.from(document.getElementsByTagName("img"), (image) => {
+        if (image.complete) return;
+
+        return new Promise((resolve, reject) => {
+          image.addEventListener("load", resolve);
+          image.addEventListener("error", reject);
+        });
+      })
+    );
+  });
+  // delay in case of images >:(
+  await timeout(5000);
   await page.pdf({
     path: "resume.pdf",
     format: "A4",
