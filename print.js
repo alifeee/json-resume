d079b742aa1a0d55f3c8ca20a5ec0bda79be5aa1
alifeee <alifeeerenn@hotmail.com>
2023-06-14 00:49:19 +0100

    replace bootstrap css to override !important so that pdf can be coloured who prints in B&W these days anyway

--- print.js
+++ print.js
@@ -16,8 +16,18 @@ async function timeout(ms) {
 
 (async () => {
   const browser = await puppeteer.launch({ headless: "new" });
+  // bootstrap has a @media print query which sets "color: black !important;". it is *very hard* to override this. So I've just copied the bootstrap css and removed it. When we make a pdf I replace the bootstrap css. Jank
+  const resumeHTML_bootstrap = await readFile("resume.html", "utf-8");
+  const replacement_bootstrap = await readFile("bootstrap.min.css");
+  const resumeHTML = resumeHTML_bootstrap.replace(
+    /<link href="https:\/\/maxcdn\.bootstrapcdn\.com\/bootstrap\/3\.3\.7\/css\/bootstrap\.min\.css" rel="stylesheet">/g,
+    `<style>
+    ${replacement_bootstrap}
+    </style>`
+  );
+
   const page = await browser.newPage();
-  await page.goto(`file://${__dirname}/resume.html`, {
+  await page.setContent(resumeHTML, {
     waitUntil: "networkidle0",
   });
   // wait for images - https://stackoverflow.com/a/49233383
