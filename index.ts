0f045b5fdcc88c8d2b61c7c5b9fd3fc9001ca127
alifeee <alifeeerenn@hotmail.com>
2023-10-11 14:16:46 +0100

    redo index.ts for a working live server

--- index.ts
+++ index.ts
@@ -1,44 +1,23 @@
-import Handlebars from "handlebars";
+import { compile } from "./build-html.ts";
+import cv from "./cv.json";
+import template from "./theme/template.txt";
+import { withHtmlLiveReload } from "bun-html-live-reload";
 
-function load_cv(): Promise<object> {
-  return Bun.file("cv.json").json();
+if (import.meta.main) {
+  Bun.serve(
+    withHtmlLiveReload({
+      port: 3000,
+      async fetch(req) {
+        const html = compile(template, cv);
+
+        return new Response(html, {
+          headers: {
+            "Content-Type": "text/html",
+          },
+        });
+      },
+    })
+  );
+
+  console.log(`Serving at http://localhost:3000/`);
 }
-
-function load_template(): Promise<string> {
-  return Bun.file("theme/template.hbs").text();
-}
-
-function compile(template_source: string, template_content: object): string {
-  const template = Handlebars.compile(template_source);
-  return template(template_content);
-}
-
-function save_html(html: string): Promise<number> {
-  return Bun.write("index.html", html);
-}
-
-const cv_json = await load_cv();
-const template_source = await load_template();
-const html = compile(template_source, cv_json);
-await save_html(html);
-
-console.log("Done!");
-
-// if main
-// if (import.meta.main) {
-//   Bun.serve({
-//     port: 3000,
-//     async fetch(req) {
-//       const cv_json = await load_cv();
-//       const template_source = await load_template();
-//       const html = compile(template_source, cv_json);
-
-//       return new Response(html, {
-//         headers: {
-//           "Content-Type": "text/html",
-//         },
-//       });
-//     },
-//   });
-//   console.log(`Serving at http://localhost:3000/`);
-// }
