eb03436551d6822be73cecc12eca045fa0cf54a5
alifeee <alifeeerenn@hotmail.com>
2023-10-13 14:47:53 +0100

    add helpers for handlebars

--- test.spec.ts
+++ test.spec.ts
@@ -1,5 +1,6 @@
-import { expect, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 import resumeSchema from "resume-schema";
+import { dateToDay, dateToMonthName, dateToYear, iconify } from "./build-html";
 
 const cv_path = "cv.json";
 const cv_file = Bun.file(cv_path);
@@ -13,5 +14,29 @@ test("resume is valid to schema", async () => {
     error = err;
     report = report;
     expect(err).toBeNull();
+  });
+});
+
+describe("helpers", () => {
+  test("dateToYear", () => {
+    expect(dateToYear("2020-12-25")).toBe("2020");
+    expect(dateToYear("1970-6-1")).toBe("1970");
+  });
+
+  test("dateToMonthName", () => {
+    expect(dateToMonthName("2020-12-25")).toBe("December");
+    expect(dateToMonthName("1970-6-1")).toBe("June");
+  });
+
+  test("dateToDay", () => {
+    expect(dateToDay("2020-12-25")).toBe("25");
+    expect(dateToDay("1970-6-1")).toBe("1");
+    expect(dateToDay("1970-6-07")).toBe("7");
+  });
+
+  test("iconify", () => {
+    expect(iconify("blog")).toBe("fas fa-rss");
+    expect(iconify("github")).toBe("fab fa-github");
+    expect(iconify("unknown")).toBe("fab fa-unknown");
   });
 });
