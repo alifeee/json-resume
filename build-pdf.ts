35b3380094ba7f8f3586cf52820ee099683aa7f9
alifeee <alifeee.web@outlook.com>
2025-03-18 16:16:43 +0000

    add `--no-sandbox` to pdf build can no longer build with GitHub actions due to Ubuntu 24.04 not having sandbox for puppeteer https://github.com/puppeteer/puppeteer/issues/13595
    since I am just using it to open a local file, no sandbox seems fine

--- build-pdf.ts
+++ build-pdf.ts
@@ -3,10 +3,13 @@ import { buildHTML } from "./build-html";
 
 export async function buildPDF() {
   await buildHTML();
-  
+
   const cv_path = new URL("./build/cv.html", import.meta.url).href;
 
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto(cv_path, { waitUntil: "networkidle0" });
   const pdf = await page.pdf({
